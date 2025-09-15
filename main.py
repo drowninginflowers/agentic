8ba2706b49dd759bcdf26b67f0ce0b6a3c8b2278
Jordyn Geiger <jordyn.geiger1@gmail.com>
2025-09-15 02:20:47 -0400

    use args

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,15 +10,31 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Generate content using Gemini API")
+    parser.add_argument("prompt", help="The prompt/content to send to the model")
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="Print extra token and prompt metadata",
+    )
+
+    args = parser.parse_args()
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=args.prompt)]),
+    ]
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph max.",
+        contents=messages,
     )
     print(response.text)
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+    if args.verbose:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print(f"User prompt: {args.prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 
 if __name__ == "__main__":
